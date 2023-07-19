9d5624f2312b83dcba2438276baf2e10e8e0902f
ajmccredie <127118760+ajmccredie@users.noreply.github.com>
2023-07-19 20:18:03 +0000

    Add check for user credentials

--- run.py
+++ run.py
@@ -8,5 +8,15 @@ password = input("Please enter your password: ")
 print(username)
 print(password)
 
+# open the user_login secure file in 'read' and check whether
+# credentials are accurate
+
+with open("user_login.txt", "r") as file:
+    for line in file:
+        valid_username, correct_password = line.strip().split(":")
+        if username == valid_username and password == correct_password:
+            print("Login successful. Welcome to your booking system!")
+        else:
+            print("Invalid username or password, please try again")
 
 

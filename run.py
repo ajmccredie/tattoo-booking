12923f8ac69661e1e30310bf42fe7257c02bde82
ajmccredie <127118760+ajmccredie@users.noreply.github.com>
2023-07-19 20:04:48 +0000

    Add user prompts for name and password

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
+
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# ask for username and password to be entered
+username = input("Please enter your username: ")
+password = input("Please enter your password: ")
+
+print(username)
+print(password)
+
+
+

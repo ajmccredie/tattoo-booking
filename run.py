ed057ae9a84895ef0021131d73670dc4dc62c899
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-08-09 16:57:10 +0000

    Update code in search function to attempt bug fix and fix typo in menu return

--- run.py
+++ run.py
@@ -348,16 +348,19 @@ def calendar_check(date_request, artist):
         # if a booking is found on that date, the nature of the booking needs to be found
         for event in events:
             summary = event.get('summary', '')
-            print(summary[12:15])
+         #   print(summary[12:15])
             booked_artist = summary[12:15]
             start_time = event['start'].get('dateTime', event['start'].get('date'))
-            print(start_time[0:10])
+       #     print(start_time[0:10])
+        #    print(f"The date required was {date_request}")
         # Compare to requested date (assuming every booking is a full day at this point)
-        if date_request == start_time:
-            date_available = False
-            print("This date is unavailable, please select another date (next available shown): \n")
-        else:
-            print("This date is available and will be used in the booking")
+            if date_request == start_time[0:10] and artist == booked_artist:
+                date_available = False
+                print("This date is unavailable, please select another date (next available shown): \n")
+            elif date_request == start_time[0:10] and not artist == booked_artist:
+                print("This date is available and will be used in the booking")
+            else:
+                continue
         if not booked_artist == "Kev":
             next_date_kev = event['start'].get('dateTime', event['start'].get('date'))[0:10]
             print(f"The next date with Kev is: {next_date_kev}")
@@ -441,7 +444,7 @@ def place_booking():
     while date_available == False:
         leave_booking = input("Do you wish to return to the main menu? y/n\n")
         if leave_booking == 'y':
-            choose_option()
+            choose_action()
             break
         else:
             artist = ask_artist_preference()
